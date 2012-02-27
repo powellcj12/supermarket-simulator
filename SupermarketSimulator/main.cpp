b055a323fc795aebe52a5249eb404de431b3e71d
Charlie Powell <powellcj12@gmail.com>
2012-02-26 21:02:50 -0500

    Initial Commit

---
+++ SupermarketSimulator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SupermarketSimulator
+//
+//  Created by Charles Powell on 2/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
