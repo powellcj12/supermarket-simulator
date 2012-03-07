ca5da789b9f72224e2bbad2f5190dcd3a8c41a18
Charlie Powell <powellcj12@gmail.com>
2012-03-07 02:53:26 -0500

    Added sample code for main.

--- SupermarketSimulator/main.cpp
+++ SupermarketSimulator/main.cpp
@@ -11,7 +11,25 @@
 using namespace std;
 
 int main ()
-{
+{  
+    int config;
+    
+    cout << "Enter configuration (1-4): ";
+    cin >> config;
+    
+    if (config >= 1 && config <= 4) {
+        Supermarket * s = new Supermarket(config);
+        
+        string filename;
+        cout << "Enter customer file name: ";
+        cin >> filename;
+        
+        s->loadCustomers(filename);
+        s->simulate();
+    }
+    else
+        cout << "Invalid configuration: " << config << endl;
+    
     return 0;
 }
 
